193968dc492ed835f939d55c9e2f6f4c926a4728
Julio Machado <jsmacbusiness@gmail.com>
2023-06-13 16:31:40 -0300

    Feat: finishing home screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import StartScreen from "./components/StartScreen";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!!</h1>
-    </div>
+    <main className="App">
+      <StartScreen />
+    </main>
   );
 }
 
