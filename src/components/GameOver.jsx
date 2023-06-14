1c572c8446958f4fc90215a239a6fe45e1529e4f
Julio Machado <jsmacbusiness@gmail.com>
2023-06-14 12:24:14 -0300

    Feat: finishing game end screen

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,9 +1,12 @@
 import "./GameOver.css";
 
-const GameOver = ({ retry }) => {
+const GameOver = ({ retry, score }) => {
   return (
     <div>
-      <h2>Game Over</h2>
+      <h2>Fim de Jogo!</h2>
+      <h3>
+        A sua pontuação foi: <span>{score}</span>
+      </h3>
       <button onClick={retry}>Recomeçar</button>
     </div>
   );
